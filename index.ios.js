226f861fba406660ea1b122748281d18900606b8
Martin Bing <bing.martin.photography@gmail.com>
2017-02-10 19:10:00 +0100

    Add dynamic location with currentlocation native option

--- index.ios.js
+++ index.ios.js
@@ -18,7 +18,7 @@ export default class GeoLocationMap extends Component {
     super(props);
 
     this.state = {
-      mapRegion: {
+      region: {
         latitude: 40.712784,
         longitude: -74.005941,
         latitudeDelta: 10,
@@ -33,17 +33,61 @@ export default class GeoLocationMap extends Component {
     };
   }
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (initialPosition) => this.setState({initialPosition}),
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+
+    this.watchID = navigator.geolocation.watchPosition((lastPosition) => {
+      this.setState({
+        latitude: lastPosition.coords.latitude,
+      });
+      this.setState({
+        longitude: lastPosition.coords.longitude,
+      });
+
+      let newRegion = {
+        latitude: lastPosition.coords.latitude,
+        longitude: lastPosition.coords.longitude,
+        latitudeDelta: 10,
+        longitudeDelta: 10,
+      };
+
+      this.setState({
+        region: newRegion,
+      });
+
+      this.setState({
+        annotations: [{
+          latitude: lastPosition.coords.latitude,
+          longitude: lastPosition.coords.longitude,
+          title: 'Current Location',
+          subtitle: 'You are here'
+        }],
+      });
+    })
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.desc}>
-          <Text style={styles.title}>
-            MAPS
+          <Text style={styles.titleLat}>
+            latitude: {this.state.latitude}
+          </Text>
+          <Text style={styles.titleLong}>
+            longitude: {this.state.longitude}
           </Text>
         </View>
         <MapView
           style={styles.map}
-          region={this.state.mapRegion}
+          region={this.state.region}
           annotations={this.state.annotations}
         />
       </View>
@@ -64,11 +108,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
+  titleLat: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 50,
+    marginTop: 25,
+  },
+  titleLong: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 75,
   },
   map: {
     position: 'absolute',
